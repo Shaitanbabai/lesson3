b358da0123e17209bff4d63fbb480c630498a00d
Shaitanbabai <rebootleo0522@gmail.com>
2024-05-04 14:40:32 +0300

    Added game cycle code
    Into cycle while added:
    screen color choice( random)
    cycle for with terms of target and mouse click match
    term for screen refresh

--- main.py
+++ main.py
@@ -25,6 +25,16 @@ color = (random.randint(a=0, b=255), random.randint(a=0, b=255), random.randint(
 # Для удобства завершение НЕ создаем бесконечный цикл while True а создаем для него переменную running
 running = True
 while running:
-    pass  # Пропускаем преждевременное исполнение цикла для исключения синтаксической ошибки
+    screen.fill(color)  # Задаем цвета экрана
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()  # Выход из игры по завершению работы прграммы, когда переменная running станет = False (vs.line 7)
