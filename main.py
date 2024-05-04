002390ce9fef09322fa3889788d33672d2a3cb02
Shaitanbabai <rebootleo0522@gmail.com>
2024-05-04 13:44:16 +0300

    All constants added
    Added target size, image and random game colors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,7 +8,19 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load('')
+icon = pygame.image.load('Images/Icon.jpg')
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load('Images/target.png')
+target_width = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(a=0, b=255), random.randint(a=0, b=255), random.randint(a=0, b=255))
+
+
 
 # Для запуска игры создан цикл while.
 # Для удобства завершение НЕ создаем бесконечный цикл while True а создаем для него переменную running
